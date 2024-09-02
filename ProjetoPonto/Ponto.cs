37a6bcdbf4e2eefae1464a7f4bf327d25e650e62
Daniel Godinho <danielgodinho923@yahoo.com.br>
2024-09-01 21:31:11 -0300

    Atividade ProjetoPonto POO

---
+++ ProjetoPonto/Ponto.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPonto
+{
+    public class Ponto
+    {
+        private int x;
+        private int y;
+        //3.21
+        public string descricao; // Novo atributo
+
+        // Propriedade para o atributo x
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        // Propriedade para o atributo y
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        // Propriedade para o atributo descricao
+        public string Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+
+        public int V1 { get; }
+        public int V2 { get; }
+
+        // metodo construtor
+        public Ponto()
+        {
+            x = 0;
+            y = 0;
+            descricao = "Sem descrição";
+        }
+
+        public Ponto(int xInicial, int yInicial, string descricaoInicial)
+        {
+            x = xInicial;
+            y = yInicial;
+            descricao = descricaoInicial; // Inicialização do novo atributo
+        }
+
+        public Ponto(int v1, int v2)
+        {
+            V1 = v1;
+            V2 = v2;
+        }
+
+        // fim do construtor
+
+        public void IncrementarCoordenadas(int deltaX, int deltaY)
+        {
+            x = x + deltaX;
+            y = y + deltaY;
+        }
+        public void ImprimirCoordenadas()
+        {
+            // imprime as coordenadas x e y e descricao
+            Console.WriteLine("Descrição: {0}, x={1}, y={2}", descricao, x, y);
+        }
+        public void ZerarCoordenadas()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Descrição: {descricao}, x={x}, y={y}";
+        }
+
+    }
+}
---
+++ ProjetoPonto/Program.cs
@@ -0,0 +1,41 @@
+﻿using ProjetoPonto;
+using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Início do main...");
+
+        ArrayList pontos = new ArrayList(); // Declara o ArrayList
+        bool continuar = true;
+
+        while (continuar)
+        {
+            Console.Write("Informe a coordenada x: ");
+            if (!int.TryParse(Console.ReadLine(), out int x) || x < 0)
+            {
+                continuar = false;
+                break;
+            }
+
+            Console.Write("Informe a coordenada y: ");
+            if (!int.TryParse(Console.ReadLine(), out int y) || y < 0)
+            {
+                continuar = false;
+                break;
+            }
+
+            Ponto ponto = new Ponto(x, y, $"Ponto ({x}, {y})");
+            pontos.Add(ponto);
+        }
+
+        Console.WriteLine($"Total de pontos instanciados: {pontos.Count}");
+        foreach (Ponto ponto in pontos)
+        {
+            Console.WriteLine(ponto.ToString());
+        }
+
+        Console.WriteLine("Fim do main...");
+    }
+}
